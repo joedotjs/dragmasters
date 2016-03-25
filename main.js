6c1c63858a22a4a2465c4fb7176e3937d88e8ae6
Joe Alves <joe@fullstackacademy.com>
2016-03-25 14:47:15 -0400

    update

--- main.js
+++ main.js
@@ -1,71 +1,28 @@
-var app = angular.module('DragMasters', []);
+var spyOn = function (func) {
+    var callCount = 0,
+        callVals  = [],
+        retVals   = [];
+    var spy = function () {
+        var args = [].slice.call(arguments);
+        var retVal = func.apply(func, args);
 
-app.controller('GameCtrl', function ($scope) {
+        //store result of applying function
+        retVals.push(retVal);
 
-    $scope.getInfo = function () {
-        return 'Joe';
+        // add arguments array values to called values array
+        callVals = callVals.concat(args);
+
+        //increment function call count by 1
+        callCount++;
+
+        //return the result of the function call
+        return retVal;
+
     };
 
-    $scope.onDrop = function (v) {
-        console.log('onDrop called with', v);
-    };
+    spy.getCallCount;
+    spy.wasCalledWith;
+    spy.returned;
 
-});
-
-app.factory('DragFactory', function () {
-
-    var value;
-
-    return {
-        set: function (v) {
-            value = v;
-        },
-        get: function () {
-            return value;
-        }
-    };
-
-});
-
-app.directive('dropzone', function ($parse, DragFactory) {
-
-    return {
-        restrict: 'A',
-        link: function (scope, element, attrs) {
-
-            var evaluateExpression = $parse(attrs.dropzone);
-
-            element.on('dragover', function (e) {
-                e.preventDefault();
-            });
-
-            element.on('drop', function (e) {
-                if (e.stopPropagation) e.stopPropagation();
-                evaluateExpression(scope, { value: DragFactory.get() });
-            });
-
-        }
-    };
-
-});
-
-app.directive('canDrag', function ($parse, DragFactory) {
-
-    return {
-        restrict: 'A',
-        link: function (scope, element, attrs) {
-
-            var evaluateExpression = $parse(attrs.canDrag);
-
-            element.attr('draggable', true);
-
-            element.on('dragstart', function () {
-                var value = evaluateExpression(scope);
-                console.log('Setting to DragFactory:', value);
-                DragFactory.set(value);
-            });
-
-        }
-    };
-
-});
+    return spy;
+};
